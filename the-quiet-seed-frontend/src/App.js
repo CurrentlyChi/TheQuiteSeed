65ba55fcd9900ade06cf68e2e6bd0d1470451d1c
MaiChi <t.maichi1510@gmail.com>
2025-03-15 09:47:46 +0900

    add stylelinttrc,postcsss

--- the-quiet-seed-frontend/src/App.js
+++ the-quiet-seed-frontend/src/App.js
@@ -1,12 +1,18 @@
-import HomePage from './pages/HomePage';
+// src/App.js
 import React from 'react';
-
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+// 今後追加するページ（例：BlogListPage）もここでインポートできます
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* 例: <Route path="/blog" element={<BlogListPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ the-quiet-seed-frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx,html}",
+      "./public/**/*.html"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          lavender: "#E6E6FA"
+        }
+      },
+    },
+    plugins: [],
+  };
+  
